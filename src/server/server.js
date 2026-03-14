c855a1fc89352b7c2c31c44a1e0329d62dd358ee
hvdroid7-pixel <hvdroid7@gmail.com>
2026-03-14 00:53:34 -0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,22 +3,27 @@ const express = require('express');
 
 const app = express();
 
-// Puerto para Render o local
 const PORT = process.env.PORT || 3000;
 
-// raíz del proyecto
 const rootDir = process.cwd();
-
-// carpeta donde están los scripts del mod
 const clientDir = path.join(rootDir, 'src', 'client');
+
+
+// ⭐ HABILITAR CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 
 // Servir scripts del mod
 app.use('/src/client', express.static(clientDir));
 
-// Servir archivos del sitio (index.html, loader.js)
+// Servir archivos del sitio
 app.use(express.static(rootDir));
 
-// Ruta principal
 app.get('/', (_req, res) => {
   res.sendFile(path.join(rootDir, 'index.html'));
 });
